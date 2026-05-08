83103bb538361e00dbff7235da46dcc5d2d90482
michaelrosa <michael_rosa28@milton.edu>
2026-05-08 14:56:08 -0400

    made classes

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,8 +1,22 @@
 public class BasicGameApp {
-    public static void main(String[] args) {}
-
     Card[] deck;
     Player p1;
     Dealer d1;
     Boolean gameon;
+
+    public static void main(String[] args) {
+        new BasicGameApp();
+    }
+
+    public BasicGameApp(){
+        System.out.println("Welcome to Blackjack!");
+        deck = new Card[52];
+
+        for(int x = 0; x<13;x=x+1){
+            deck[x] = new Card(10,x,"hearts");
+            deck[x].print();
+        }
+    }
+
+
 }
--- src/Card.java
+++ src/Card.java
@@ -4,4 +4,22 @@ public class Card {
     String name;
     String suit;
 
+    public Card(int pValue, int pName , String pSuit){
+    value = pValue;
+    if(pName ==0){
+        name = "ace";
+        value = 11;
+    } else if (pName ==1){
+        name = "two";
+        value = 2;
+    }
+
+    suit = pSuit;
+    }
+
+    public void print(){
+        System.out.println("The "+name+" of "+suit+" is worth "+ value+" points ");
+
+    }
+
 }
