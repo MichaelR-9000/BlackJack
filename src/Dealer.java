83717b0f43e36e55aec616a9c3f0dbd0bfbbc1b5
michaelrosa <michael_rosa28@milton.edu>
2026-05-15 08:06:28 -0400

    made methods

--- src/Dealer.java
+++ src/Dealer.java
@@ -5,4 +5,31 @@ public class Dealer {
     Card[]hand;
     Boolean isbust;
 
+    public Dealer(){
+
+        Isover16 = false;
+        CardTotal = 0;
+        hand = new Card[10];
+        isbust = false;
+
+    }
+
+    public void printInfo(){
+        System.out.println("Over 16: " + Isover16);
+        System.out.println("Card Total: " + CardTotal);
+        System.out.println("Bust: " + isbust);
+        for(int i = 0; i < hand.length; i = i + 1){
+
+            if(hand[i] == null){
+
+            }
+            else{
+                hand[i].print();
+            }
+        }
+    }
+
 }
+
+
+
--- src/Player.java
+++ src/Player.java
@@ -9,10 +9,30 @@ public class Player {
     //todo: make constructor with no parameters that sets the defaults for ten instance variavles
     //todo: make a print that prints a;; the instanmce cariables
 
-    public Player(){
-        Cardtotal = 52;
+    public Player() {
+        Cardtotal = 0;
         isBust = false;
         Ishit = false;
+        hand = new Card[10];
+        name = "Mike";
+    }
 
+    public void printInfo() {
+        System.out.println("Card Total: " + Cardtotal);
+        System.out.println("Bust: " + isBust);
+        System.out.println("Hit: " + Ishit);
+        System.out.println("name: " + name);
+
+        for (
+                int i = 0;
+                i < hand.length; i = i + 1) {
+
+            if (hand[i] == null) {
+
+            } else {
+                hand[i].print();
+
+            }
+        }
     }
 }
